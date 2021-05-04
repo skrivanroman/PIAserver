c79bc7ad5fe3dd782d9a9736e1aabe971626ae3a
skrivanroman <romanskrivan12@gmail.com>
2021-05-04 15:47:19 +0200

    mock up

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.send('<img src="/images/test.png">')
+    res.send('<img src="./images/test.png">')
     //res.render('index', { title: 'Express' })
 })
 
